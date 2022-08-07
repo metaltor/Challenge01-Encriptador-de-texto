b4777fb4fd1a16e6384fb9eebca8be3b99855aca
metaltor <gtn.280693@gmail.com>
2022-08-07 16:19:38 -0500

    programa funcionando
    todas las funcionalidades estan habilitadas

--- scripts.js
+++ scripts.js
@@ -1,41 +1,6 @@
 document.querySelector('#encriptar').onclick = encriptar
 document.querySelector('#desencriptar').onclick = desencriptar
-
-function validador(texto){
-    if(/([^a-z\ñ]\s*)/g.test(texto) === false) {
-        return true
-    }else{
-        return false
-    }
-}
-function pikachuTriste(){
-    document.querySelector('#pikachu-feliz').src="images/pikachu-triste.png";
-    document.querySelector('#mensaje2').classList.add('hiden');
-    document.querySelector('#mensaje1').textContent = "Ingresa solo minusculas...";
-    document.querySelector('#mensaje1').classList.add('alerta-texto');
-    document.querySelector('#alerta-minusculas').classList.add('alerta-fondo');
-}
-function pikachuFeliz(){
-    document.querySelector('#pikachu-feliz').src="images/pikachu-feliz.png";
-    document.querySelector('#mensaje2').classList.remove('hiden');
-    document.querySelector('#mensaje1').textContent = "Ningún mensaje fue encontrado";
-    document.querySelector('#mensaje1').classList.remove('alerta-texto');
-    document.querySelector('#alerta-minusculas').classList.remove('alerta-fondo');
-    document.querySelector('#texto-ingresado').value =''
-}
-function pikachuVacio(){
-    document.querySelector('#pikachu-feliz').src="images/pikachu-triste.png";
-    document.querySelector('#mensaje2').classList.add('hiden');
-    document.querySelector('#mensaje1').textContent = "No ingresaste nada.";
-    document.querySelector('#mensaje1').classList.add('alerta-texto');
-}
-
-function imprimirResultado(texto){
-    document.querySelector('#pikachu-feliz').classList.add('hiden')
-    document.querySelector('#mensaje1').classList.add('hiden');
-    document.querySelector('#mensaje2').textContent = texto;
-    
-}
+document.querySelector('#boton-copiar').onclick =copiarAlPortapapeles 
 function encriptar(event){
     event.preventDefault()
     const $formulario = document.querySelector('#texto-para-encriptar');
@@ -45,6 +10,7 @@ function encriptar(event){
         .split('')
         .map(letra => letra === "a" ? letra = "ai" : letra === 'e' ? letra = 'enter': letra === 'i' ? letra = 'imes' : letra === 'o' ? letra = 'ober' : letra === 'u' ? letra = 'ufat' : letra)
         .join('');
+        mostrarBotonCopiar()
        imprimirResultado(nuevoTexto) 
         
     } else if (texto === ''){
@@ -61,14 +27,75 @@ function encriptar(event){
     }
    
 }
-// function desencriptar(texto){
-//     console.log (texto)
-    function desencriptar(){ 
-        pikachuFeliz();
-    // texto.replace(/imes/igm, "i"); 
-    // texto = texto.replace(/ai/igm, "a"); 
-    // texto = texto.replace(/enter/igm, "e"); 
-    // texto = texto.replace(/ober/igm, "o"); 
-    // texto = texto.replace(/ufat/igm, "u");
-    //     return texto
+function desencriptar(event){
+    event.preventDefault();
+    const $formulario = document.querySelector('#texto-para-encriptar');
+    let texto = $formulario['texto-ingresado'].value;
+    if(validador(texto) && texto !=''){
+    texto.replace(/imes/igm, "i"); 
+    texto = texto.replace(/ai/igm, "a"); 
+    texto = texto.replace(/enter/igm, "e"); 
+    texto = texto.replace(/ober/igm, "o"); 
+    texto = texto.replace(/ufat/igm, "u");
+    mostrarBotonCopiar()
+    imprimirResultado(texto) 
+    }else if (texto === ''){
+        pikachuVacio()
+        setTimeout(() => {
+            pikachuFeliz()
+        }, 2000);
+
+    }else {
+        pikachuTriste()
+        setTimeout(() => {
+            pikachuFeliz()
+        }, 2000);
+    }
+        
+}
+function copiarAlPortapapeles(){
+    const $textoResultado = document.querySelector('#mensaje2').textContent;
+    navigator.clipboard.writeText($textoResultado).then(() => {  
+    }, () => {    
+    });
+}
+
+function validador(texto){
+    if(/([^a-z\ñ\s])/g.test(texto) === false) {
+        return true
+    }else{
+        return false
+    }
+}
+function pikachuTriste(){
+    document.querySelector('#pikachu').src="images/pikachu-triste.png";
+    document.querySelector('#mensaje2').classList.add('hiden');
+    document.querySelector('#mensaje1').textContent = "Ingresa solo minusculas...";
+    document.querySelector('#mensaje1').classList.add('alerta-texto');
+    document.querySelector('#alerta-minusculas').classList.add('alerta-fondo');
+}
+function pikachuFeliz(){
+    document.querySelector('#pikachu').src="images/pikachu-feliz.png";
+    document.querySelector('#mensaje2').classList.remove('hiden');
+    document.querySelector('#mensaje1').textContent = "Ningún mensaje fue encontrado";
+    document.querySelector('#mensaje1').classList.remove('alerta-texto');
+    document.querySelector('#alerta-minusculas').classList.remove('alerta-fondo');
+    document.querySelector('#texto-ingresado').value =''
+}
+function pikachuVacio(){
+    document.querySelector('#pikachu').src="images/pikachu-triste.png";
+    document.querySelector('#mensaje2').classList.add('hiden');
+    document.querySelector('#mensaje1').textContent = "No ingresaste nada.";
+    document.querySelector('#mensaje1').classList.add('alerta-texto');
+}
+
+function imprimirResultado(texto){
+    document.querySelector('#pikachu').classList.add('hiden')
+    document.querySelector('#mensaje1').classList.add('hiden');
+    document.querySelector('#mensaje2').textContent = texto;
+    
+}
+function mostrarBotonCopiar(){
+document.querySelector('#boton-copiar').style.visibility = 'visible'
+document.querySelector('#texto-resultado').classList.add('resultado');
 }
